cf06497c4e102ec32c41bb688a96746ae0d1e659
Andi <andreas.braun98@outlook.de>
2021-04-17 00:27:50 +0000

    added bars and changed colors

--- config.h
+++ config.h
@@ -1,26 +1,26 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int gappx     = 8;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "arial:size=14" };
-static const char dmenufont[]       = "arial:size=14";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#0055ff";
+static const char *fonts[]          = { "fontawesome:size=15", "fontawesome:size=15" };
+static const char dmenufont[]       = "SourceCodePro-Medium:size=15";
+static const char col_gray1[]       = "#263238";
+static const char col_gray2[]       = "#263238";
+static const char col_gray3[]       = "#90A4AE";
+static const char col_gray4[]       = "#ECEFF1";
+static const char col_cyan[]        = "#263238";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
-/* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+/* tagging */				 // Terminal,	Code	Firefox,	FileManager
+static const char *tags[] = {	"",		"",	"",		"", 			"5", "6", "7", "8", "9"};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -62,9 +62,9 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 // custom commands
-static const char *raisevol[] = { "/home/andi/tools/suckless/avd/cmd/raisevol-amixer", NULL};
-static const char *lowervol[] = { "/home/andi/tools/suckless/avd/cmd/lowervol-amixer", NULL};
-static const char *mutevol[]  = { "/home/andi/tools/suckless/avd/cmd/mutevol-amixer", NULL};
+static const char *raisevol[] = { "/home/andi/Tools/suckless/avd/cmd/raisevol-amixer", NULL};
+static const char *lowervol[] = { "/home/andi/Tools/suckless/avd/cmd/lowervol-amixer", NULL};
+static const char *mutevol[]  = { "/home/andi/Tools/suckless/avd/cmd/mutevol-amixer", NULL};
 
 
 #include <X11/XF86keysym.h>
