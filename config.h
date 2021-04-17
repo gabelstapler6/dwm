5514bae290a761980bcededf61d75d63e9d88411
Andi <andreas.braun98@outlook.de>
2021-04-17 02:10:36 +0000

    clients spawn now aside the master

--- config.h
+++ config.h
@@ -19,7 +19,7 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
-static const char *tags[] = {			"",  "",		 "",	 "",    "", 		"6", "7", "8", "9"};
+static const char *tags[] = {			"",  "",		 "",	 "",    "", 		"6", "7", "8", "9"};
 static const char *defaulttagapps[] = { "st", "firefox", "code", "nemo", "spotify", NULL, NULL, NULL, NULL };
 
 static const Rule rules[] = {
@@ -87,7 +87,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_p,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_x, 	   zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,             			XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
