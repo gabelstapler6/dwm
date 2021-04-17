1f5da49da3b5fc96b48b41b45156a739e923ce8c
Andi <andreas.braun98@outlook.de>
2021-04-17 02:17:55 +0000

    added border alpha patch

--- config.h
+++ config.h
@@ -13,10 +13,20 @@ static const char col_gray2[]       = "#263238";
 static const char col_gray3[]       = "#CFD8DC";
 static const char col_gray4[]       = "#009688";
 static const char col_cyan[]        = "#263238";
+
+static const unsigned int baralpha = 0xd0;
+static const unsigned int borderalpha = OPAQUE;
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+};
+
+static const unsigned int alphas[][3]      = {
+	/*               fg      bg        border     */
+	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 static const char *tags[] = {			"",  "",		 "",	 "",    "", 		"6", "7", "8", "9"};
